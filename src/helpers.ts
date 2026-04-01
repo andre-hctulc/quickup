8e5d6146430a49fbd15959b819c8171ded11c63a
andre-hctulc <andre.tho.44@gmail.com>
2026-04-01 19:00:02 +0200

    >>

--- src/helpers.ts
+++ src/helpers.ts
@@ -9,45 +9,29 @@ export function zodVar<T>(value: any, schema: z.ZodType<T>, varName?: string): T
     }
 }
 
-export function zodEnvVar<T>(varName: string, schema: z.ZodType<T>): T {
-    return zodVar(process.env[varName], schema, varName);
+export function zodEnvVar<T>(varName: string, schema: z.ZodType<T>, defaultValue?: T): T {
+    return zodVar(process.env[varName] || defaultValue, schema, varName);
 }
 
 /**
  * Environment variable value. Does not allow empty values.
  */
 export function envVar(varName: string, defaultValue?: string): string {
-    return zodEnvVar(
-        varName,
-        defaultValue === undefined ? z.string().min(1) : z.string().min(1).default(defaultValue),
-    );
-}
-
-/**
- * Environment variable value. Allows empty values.
- */
-export function envVarOpt(varName: string): string {
-    return zodEnvVar(varName, z.string().optional().default(""));
+    return zodEnvVar(varName, z.string().min(defaultValue === "" ? 0 : 1), defaultValue);
 }
 
 /**
  * Attempt to parse an environment variable to an integer.
  */
 export function envVarInt(varName: string, defaultValue?: number): number {
-    return zodEnvVar(
-        varName,
-        defaultValue === undefined ? z.coerce.number().int() : z.coerce.number().int().default(defaultValue),
-    );
+    return zodEnvVar(varName, z.coerce.number().int(), defaultValue);
 }
 
 /**
  * Attempt to parse an environment variable to a number.
  */
 export function envVarNum(varName: string, defaultValue?: number): number {
-    return zodEnvVar(
-        varName,
-        defaultValue === undefined ? z.coerce.number() : z.coerce.number().default(defaultValue),
-    );
+    return zodEnvVar(varName, z.coerce.number(), defaultValue);
 }
 
 /**
